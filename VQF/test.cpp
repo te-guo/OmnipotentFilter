ad8145df85cea34bf99e98877f2e2e87be176341
te-guo <teguo@outlook.com>
2021-11-12 21:16:37 +0800

    init VQF

---
+++ VQF/test.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<chrono>
+#include<vector>
+#include "vqf_filter.h"
+#include "hashing.h"
+#include "evaluation.h"
+using namespace std;
+typedef long long ll;
+const int N = 23;
+
+class VqfEvaluation : public EvaluationBase {
+	virtual void init() {
+		F = vqf_init(1 << N);
+	}
+	virtual bool insert(char *key) {
+		return vqf_insert(F, hash_func1_32bit(key));
+	}
+	virtual bool query(char *key) {
+		return vqf_is_present(F, hash_func1_32bit(key));
+	}
+	virtual bool remove(char *key) {
+		return vqf_remove(F, hash_func1_32bit(key));
+	}
+	virtual void debug() {
+	}
+	vqf_filter* F;
+} E;
+int main(int argc, char* argv[]) {
+	
+	int n = (1<<N);
+	auto data = gen_random_data(n);
+	//for (int i=0; i<100; i++) cout<<data[i].key<<endl;
+	E.data = data;
+	E.evaluation();
+	return 0;
+}
