30fe9de99f3c8ca1ae12a6621cf344781dfbf9d0
fizzydavid <fizzydavid@qq.com>
2021-12-03 12:22:43 +0800

    add Murmurhash3

--- include/hashing.cpp
+++ include/hashing.cpp
@@ -1,10 +1,26 @@
 #include "hashing.h"
+#include "MurmurHash3.h"
+#include <cstring>
 
 uint64_t _random_seed = 0x8091a2b3c4d5e6f7;
 void set_random_seed(uint64_t seed) {  // A fixed pseudo-random sequence with respect to the seed should be produced after calling this method
 	_random_seed = seed;
 }
 
+uint64_t* hash_func_64bit(uint64_t key) {
+	uint64_t ret[2];
+	MurmurHash3_x64_128(&key, 8, _random_seed, ret);
+	return ret;
+}
+
+uint64_t* hash_func_64bit(const char* key) {
+	uint64_t ret[2];
+	MurmurHash3_x64_128(key, strlen(key), _random_seed, ret);
+	return ret;
+}
+
+
+// The following hash functions are to be discarded 
 uint32_t hash_func1_32bit(const char* key) {
 	uint64_t hash_value = _random_seed;
 	for (int i=0; key[i]; i++) {
